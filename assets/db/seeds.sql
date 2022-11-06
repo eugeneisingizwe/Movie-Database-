d3beedc70efed0c6aac35efe17365401ce08b10f
eugene isingizwe <eugeneisingizwe95@hotmail.com>
2022-11-06 13:32:53 -0700

    intial commit

---
+++ assets/db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO movies (movie_name)
+VALUES ("Rush Hour 1"),
+       ("Friday"),
+       ("Boyz In The Hood"),
+       ("Batman vs Superman"),
+       ("Beast Of no nation"),
+       ("Zootopia");
+
+INSERT INTO reviews (movie_id, reviews)
+VALUES (1, "Rush Hour is the funniest movie of all tim. It is trueky underrated. Give that hornbill a sequel!"),
+       (2, "Smokeyyy I love Frieday!, watch this movie"),
+       (1, "Chrst Tucher is the funniest comedian alive"),
+       (3, "Ten years of ballet and three years of tap to join a gang in this neighborhood"),
+       (4, "Superman can easly kill batman lol great movie overrall"),
+       (1, "Jackie and Christ are the perfect combination"),
+       (5, "What a great and inspiring story! Movie of the year"),
+       (6, "hahah I love this movie");
